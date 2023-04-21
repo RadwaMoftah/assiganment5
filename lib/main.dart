48a9858c833ca4a5ecb67d3c32ae1c96ce1b6055
RadwaMoftah <rm1919@fayoum.edu.eg>
2023-04-21 20:21:11 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,179 +11,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(),
-      home: const MyHomePage(title: 'Flutter Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter1 = 0;
-  int _counter2 = 0;
-
-  void _incrementCounter1() {
-    setState(() {
-      _counter1++;
-    });
-  }
-
-  void _incrementCounter1_2() {
-    setState(() {
-      _counter1 += 2;
-    });
-  }
-
-  void _incrementCounter1_3() {
-    setState(() {
-      _counter1 += 3;
-    });
-  }
-
-  void _incrementCounter2() {
-    setState(() {
-      _counter2++;
-    });
-  }
-
-  void _incrementCounter2_2() {
-    setState(() {
-      _counter2 += 2;
-    });
-  }
-
-  void _incrementCounter2_3() {
-    setState(() {
-      _counter2 += 3;
-    });
-  }
-
-  void _reset() {
-    setState(() {
-      _counter1 = 0;
-      _counter2 = 0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 0, 255, 89),
-        title: Text('Basketball Score Counter'),
-      ),
-      body: Column(
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'TEAM A',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '$_counter1',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: _incrementCounter1,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 0, 255, 89)),
-                      child: Text(
-                        '+1 POINT',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: _incrementCounter1_2,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 0, 255, 89)),
-                      child: Text(
-                        '+2 POINT',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: _incrementCounter1_3,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 0, 255, 89)),
-                      child: Text(
-                        '+3 POINT',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                )
-              ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 47, 133, 100),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/images/roro.jpg'),
+              radius: 85,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'TEAM B',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '$_counter2',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: _incrementCounter2,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 0, 255, 89)),
-                      child: Text(
-                        '+1 POINT',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: _incrementCounter2_2,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 0, 255, 89)),
-                      child: Text(
-                        '+2 POINT',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: _incrementCounter2_3,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 0, 255, 89)),
-                      child: Text(
-                        '+3 POINT',
-                        style: TextStyle(color: Colors.white),
-                      )),
-                )
-              ],
+            Text(
+              "Radwa Moftah",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Schyler',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
-          ]),
-          OutlinedButton(
-            onPressed: _reset,
-            child: Text(
-              'RESET',
-              style: TextStyle(fontSize: 20),
+            Text(
+              'SOFTWARE ENGINEER',
+              style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromARGB(255, 66, 188, 142)),
             ),
-          )
-        ],
+            Divider(
+              color: Colors.white,
+              thickness: 2,
+              indent: 50,
+              endIndent: 50,
+            ),
+            Card(
+              color: Colors.white,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            Container(
+              child: ListTile(
+                title: Text("+2 0115475216"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Color.fromARGB(255, 47, 133, 100),
+                ),
+                selectedTileColor: Colors.white,
+              ),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15),
+            ),
+            Container(
+              child: ListTile(
+                title: Text("RM1919@fayoum.edu.eg"),
+                leading: Icon(
+                  Icons.email,
+                  color: Color.fromARGB(255, 47, 133, 100),
+                ),
+                selectedTileColor: Colors.white,
+              ),
+              color: Colors.white,
+              margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
+            )
+          ],
+        ),
       ),
     );
   }
